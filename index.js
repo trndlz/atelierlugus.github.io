48292b95ef81e60fba07de0fdd43c3c9669c1b05
Trestan Mervin <tmervin@mediarithmics.com>
2021-09-20 22:06:36 +0200

    Added style and fixed chrome compatibilty

--- index.js
+++ index.js
@@ -1,33 +1,79 @@
 const video = document.getElementById("video");
 const videoContainer = document.getElementById("video-container");
 
-document.exitFullscreen().then(() => {
-  video.style.display = "none";
-});
+// if (document.fullscreenElement) {
+//   document.exitFullscreen().then(() => {
+//     console.log("EXIT")
+//     video.style.display = "none";
+//     video.load();
+//   });
+// }
 
-function toggleFullScreen() {
-  console.log("YO");
-  if (document.fullscreenElement) {
-    document.exitFullscreen().then(() => {
-      video.style.display = "none";
-    });
-  } else if (document.webkitFullscreenElement) {
-    // Need this to support Safari
-    document.webkitExitFullscreen();
-  } else if (videoContainer.webkitRequestFullscreen) {
-    // Need this to support Safari
-    video.webkitRequestFullscreen();
-  } else {
-    video.requestFullscreen().then(() => {
-      video.load();
-      video.style.display = "block";
-      video.play();
-    });
-  }
-}
+// function toggleFullScreen() {
+//   if (document.fullscreenElement) {
+//     console.log("A")
+//     document.exitFullscreen().then(() => {
+//       console.log("EXIT 222")
+//       video.style.display = "none";
+//     });
+//   } else if (document.webkitFullscreenElement) {
+//     console.log("B")
+//     // Need this to support Safari
+//     document.webkitExitFullscreen();
+//   } else if (videoContainer.webkitRequestFullscreen) {
+//     console.log("C")
+//     // Need this to support Safari
+//     video.webkitRequestFullscreen();
+//     video.load();
+//       console.log( video.style.display)
+//       // video.style.display = "flex";
+//       // console.log( video.style.none)
+//       video.play()
+//   } else {
+//     video.requestFullscreen().then(() => {
+//       video.load();
+//       console.log( video.style.display)
+//       video.style.display = "flex";
+//       console.log( video.style.display)
+//       video.play();
+//     });
+//   }
+// }
 
-document.addEventListener("fullscreenchange", function (event) {
-  if (!document.fullscreen) {
+document.onfullscreenchange = function ( event ) {
+  console.log("FULL SCREEN CHANGE", document.fullscreenElement);
+  if (!document.fullscreenElement) {
     video.style.display = "none";
   }
-});
+};
+
+function toggleFullScreen() {
+  if (document.fullscreenEnabled) {
+    if (!document.fullscreenElement) {
+      video.requestFullscreen().then(() => {
+        video.style.display = "flex";
+        video.load();
+        video.play();
+      });
+  } else {
+    if (document.exitFullscreen) {
+      document.exitFullscreen().then(() => {
+        console.log(">>>>>")
+        video.style.display = "none";
+      });
+    } else {
+      video.style.display = "none";
+    }
+  }
+  } else {
+    window.alert('Sorry, your browser is not compatible with full screen mode !')
+  }
+  
+}
+
+
+const hooligan = ["Allez, Lorient, allez !", "Visitez https://fclweb.fr/", "Lorient est hauuuuuuuuuuuuuuuuuuuuut !" ];
+
+hooligan.forEach((h) => {
+  console.log(`%c ${h}`, 'background: #ffa500');
+})
